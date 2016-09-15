9929e4d8ea9c3969c8cc5c819302a9927012a786
Obodugo Rapheal <kofacts@gmail.com>
2016-09-14 15:07:32 -1100

    Update header.php

--- header.php
+++ header.php
@@ -3,12 +3,14 @@
 <head>
 	<title>Digitalise Template for Digital Agencies</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" type="text/css" href="./assets/bs/css/bootstrap.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
   <link rel="stylesheet" type="text/css" href="./assets/latofonts.css">
   <link rel="stylesheet" type="text/css" href="./assets/style.css">
   <link rel="stylesheet" type="text/css" href="./assets/font-awesome.css">
-  <script type="text/javascript" src="./assets/bs/js/bootstrap.js"></script>
-  <script type="text/javascript" src="./assets/jquery.min.js"></script>
+  
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
   <script type="text/javascript" src="./assets/scrollreveal.min.js"></script>
   <script type="text/javascript" src="autoscroll.js"></script>
   <style type="text/css">
