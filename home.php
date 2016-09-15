c56cf3ff70ecf486e4ebc17aa525895d8cc84666
Obodugo Rapheal <kofacts@gmail.com>
2016-09-14 15:04:42 -1100

    Update home.php

--- home.php
+++ home.php
@@ -5,7 +5,7 @@ include "header.php";
 //check if my db is connected
 $server="localhost";
 $name="root";
-$password="pico4421";
+$password=""; //change to yours or leave field is no password. 
 $db_name="digitalise"; //You can change this based on your DB structure.
 $connect= new mysqli($server,$name,$password,$db_name);
 //check if it is connected.
